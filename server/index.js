79f06493899fdbd6ef4105f925da50ca0ed96d8b
ankitkothe2119 <135228960+ankitkothe2119@users.noreply.github.com>
2023-11-22 09:39:03 +0000

    create foler stu

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import  AuthRoute  from "./Routes/AuthRoute.js";
 
-
+//Routes
 
 const app = express();
+
+//Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 
+dotenv.config()
 
-mongoose.connect(
-    "mongodb+srv://mrak090:2119ankit@ankit9100.yacutxw.mongodb.net/",
-    {useNewUrlParser: true, useUnifiedTopology: true}
-    ).then(() => app.listen(4000, () => console.log("Listening")));
+mongoose
+.connect(process.env.MONGO_DB,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+.then(() => app.listen(process.env.PORT, () => console.log(`Listening ${PORT}`))).catch((error)=>console.log(error))
+
+// usage of routes
+
+app.use("/auth",AuthRoute)
